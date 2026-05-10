0b71edc27975363100f5e8d909eda0e1fa79f932
abstark-3000 <149358036+abstark-3000@users.noreply.github.com>
2026-05-10 18:22:06 +0530

    Fixed deployment configuration

--- backend/app.py
+++ backend/app.py
@@ -23,5 +23,5 @@ def recommend_api():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port)
